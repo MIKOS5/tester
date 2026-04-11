ffcd11575f033010cc0ac756d07c012c046bf7bb
MIKOS5 <mikos666@gmail.com>
2026-04-10 23:16:18 -0700

    Create pending.js

---
+++ pending.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Pending Battles</title>
+</head>
+<body>
+
+<h1>Pending Players</h1>
+
+<div id="list"></div>
+
+<script type="module" src="assets/js/pending.js"></script>
+
+</body>
+</html>
