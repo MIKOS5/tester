5b27681a7a2132853553282d60c39af942529ea7
MIKOS5 <mikos666@gmail.com>
2026-06-21 18:42:21 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,21 @@
-
 const CACHE_NAME = "radiowave-cache-v1";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
+const FILES = [
+  "./index99.html",
   "./manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
