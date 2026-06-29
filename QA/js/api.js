3aa944a50522ea2d66976bd4d083fb90e719351e
MIKOS5 <mikos666@gmail.com>
2026-06-28 20:27:10 -0700

    Update api.js

--- QA/js/api.js
+++ QA/js/api.js
@@ -2,17 +2,16 @@ const API_BASE = "https://script.google.com/macros/s/AKfycbzcjIipaNkV_TbzV7XswF8
 
 async function api(action, payload = {}) {
   try {
-    const url = `${API_BASE}?action=${encodeURIComponent(action)}`;
 
-    const res = await fetch(url, {
+    const res = await fetch(API_BASE, {
       method: "POST",
-
-      // IMPORTANT: avoids Google Apps Script CORS preflight issues
       headers: {
         "Content-Type": "text/plain;charset=utf-8"
       },
-
-      body: JSON.stringify(payload)
+      body: JSON.stringify({
+        action,
+        ...payload
+      })
     });
 
     if (!res.ok) {
